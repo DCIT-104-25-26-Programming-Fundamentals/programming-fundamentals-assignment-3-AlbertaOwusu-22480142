98cd86b6de7773ae0f49a79d3e2abe8a7debba2d
Alberta Owusu <aadowusu@st.ug.edu.gh>
2026-08-07 04:13:22 +0000

    completed Q4

--- assignment_04_matrix_operations.js
+++ assignment_04_matrix_operations.js
@@ -70,3 +70,127 @@
 
 const readlineSync = require('readline-sync');
 
+function readMatrix(r, c, who) {
+    let m = [];
+    for (let i = 0; i < r; i++) {
+        let line = readlineSync.question('Enter row ' + (i + 1) + who + ': ');
+        let row = line.split(' ').map(Number);
+        m.push(row);
+    }
+    return m;
+}
+
+function showMatrix(m) {
+    for (let i = 0; i < m.length; i++) {
+        console.log(m[i].join('  '));
+    }
+}
+
+function transpose(m) {
+    let r = m.length;
+    let c = m[0].length;
+    let res = [];
+    for (let j = 0; j < c; j++) {
+        let newRow = [];
+        for (let i = 0; i < r; i++) {
+            newRow.push(m[i][j]);
+        }
+        res.push(newRow);
+    }
+    return res;
+}
+
+function addMatrix(a, b) {
+    let r = a.length;
+    let c = a[0].length;
+    let res = [];
+    for (let i = 0; i < r; i++) {
+        let newRow = [];
+        for (let j = 0; j < c; j++) {
+            newRow.push(a[i][j] + b[i][j]);
+        }
+        res.push(newRow);
+    }
+    return res;
+}
+
+function multiplyMatrix(a, b) {
+    let r1 = a.length;
+    let c1 = a[0].length;
+    let c2 = b[0].length;
+    let res = [];
+    for (let i = 0; i < r1; i++) {
+        let newRow = [];
+        for (let j = 0; j < c2; j++) {
+            let total = 0;
+            for (let k = 0; k < c1; k++) {
+                total += a[i][k] * b[k][j];
+            }
+            newRow.push(total);
+        }
+        res.push(newRow);
+    }
+    return res;
+}
+
+function doTranspose() {
+    let r = readlineSync.questionInt('Enter number of rows: ');
+    let c = readlineSync.questionInt('Enter number of columns: ');
+    let m = readMatrix(r, c, '');
+
+    console.log('\nOriginal Matrix:');
+    showMatrix(m);
+
+    console.log('\nTransposed Matrix:');
+    showMatrix(transpose(m));
+}
+
+function doAddition() {
+    let r = readlineSync.questionInt('Enter number of rows: ');
+    let c = readlineSync.questionInt('Enter number of columns: ');
+
+    console.log('\nMatrix A:');
+    let a = readMatrix(r, c, ' of A');
+
+    console.log('\nMatrix B:');
+    let b = readMatrix(r, c, ' of B');
+
+    console.log('\nSum:');
+    showMatrix(addMatrix(a, b));
+}
+
+function doMultiplication() {
+    let r1 = readlineSync.questionInt('Enter rows of A: ');
+    let c1 = readlineSync.questionInt('Enter columns of A: ');
+
+    console.log('\nMatrix A:');
+    let a = readMatrix(r1, c1, ' of A');
+
+    let r2 = c1;
+    let c2 = readlineSync.questionInt('Enter columns of B: ');
+
+    console.log('\nMatrix B:');
+    let b = readMatrix(r2, c2, ' of B');
+
+    console.log('\nProduct:');
+    showMatrix(multiplyMatrix(a, b));
+}
+
+function main() {
+    console.log('A - Transpose a Matrix');
+    console.log('B - Add Two Matrices');
+    console.log('C - Multiply Two Matrices');
+    let choice = readlineSync.question('Choose an option (A/B/C): ').toUpperCase();
+
+    if (choice === 'A') {
+        doTranspose();
+    } else if (choice === 'B') {
+        doAddition();
+    } else if (choice === 'C') {
+        doMultiplication();
+    } else {
+        console.log('Invalid choice.');
+    }
+}
+
+main();
